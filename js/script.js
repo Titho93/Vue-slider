deb6b4b4e42da233ff400cc64753c9fe72234f06
titho93 <titho93.10@gmail.com>
2023-09-28 15:45:43 +0200

    [add]counter and button

--- js/script.js
+++ js/script.js
@@ -5,15 +5,27 @@ createApp ({
     data () {
 
         return {
+            
             images: [
                 '/img/01.webp',
                 '/img/02.webp',
                 '/img/03.webp',
                 '/img/04.webp',
                 '/img/05.webp'
-            ]
-           
+            ],
+            
+            counter: 0
         }
+
+    },
+    methods:{
+        nextPrev(isNext){
+            if(isNext){
+                this.counter++;
+            }else{
+                this.counter--;
+            }
+        }    
 
     }
     
