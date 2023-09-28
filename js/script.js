dcadfd0ddc615d12955c834a9ee90ee4593fc38c
titho93 <titho93.10@gmail.com>
2023-09-28 16:58:23 +0200

    [fix]length bugs

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ createApp ({
         return {
             
             images: [
-                '/img/01.webp',
-                '/img/02.webp',
-                '/img/03.webp',
-                '/img/04.webp',
-                '/img/05.webp'
+                './img/01.webp',
+                './img/02.webp',
+                './img/03.webp',
+                './img/04.webp',
+                './img/05.webp'
             ],
             
             counter: 0
@@ -26,10 +26,10 @@ createApp ({
                 this.counter--;
             };
 
-            if(this.counter === this.images.lenght){
+            if(this.counter === this.images.length){
                 this.counter = 0;
             }else if(this.counter < 0){
-                this.counter = this.images.lenght -1;
+                this.counter = this.images.length -1;
             }
         }
         
